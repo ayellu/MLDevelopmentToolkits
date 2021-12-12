e44bfc2858c0a1ed344c3ca4cd1fcad6c1328a2b
Ashwini Yellu <ayellu@umass.edu>
2021-12-12 11:55:12 -0500

    removing append tags

--- monitor_ml_runs_live.py
+++ monitor_ml_runs_live.py
@@ -121,9 +121,6 @@ results = model.fit(
 #         epochs=PARAMS['epoch_nr'],
 #         batch_size=PARAMS['batch_size'],
 #         callbacks=[NeptuneMonitor()])
-import os
-if os.getenv('CI') == "true":
-   neptune.append_tag('ci-pipeline', os.getenv('Experiment_1'))
 
 neptune.stop()
 
